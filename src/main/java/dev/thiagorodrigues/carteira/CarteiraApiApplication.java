4fd4363d87d090f970b079a20f317fc6caeb547c
Thiago Rodrigues <thiagor_@live.com>
2021-09-13 12:51:16 -0300

    chore: create project

---
+++ src/main/java/dev/thiagorodrigues/carteira/CarteiraApiApplication.java
@@ -0,0 +1,13 @@
+package dev.thiagorodrigues.carteira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteiraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteiraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/thiagorodrigues/carteira/CarteiraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.thiagorodrigues.carteira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteiraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
