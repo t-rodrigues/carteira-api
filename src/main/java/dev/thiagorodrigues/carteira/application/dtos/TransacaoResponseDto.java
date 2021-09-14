b0e15ed7003d81f616cc6a2db8b63c75fceb91dd
Thiago Rodrigues <thiagor_@live.com>
2021-09-13 21:00:17 -0300

    feat: add Lombok

--- src/main/java/dev/thiagorodrigues/carteira/application/dtos/TransacaoResponseDto.java
+++ src/main/java/dev/thiagorodrigues/carteira/application/dtos/TransacaoResponseDto.java
@@ -4,7 +4,11 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import dev.thiagorodrigues.carteira.domain.entities.enums.TipoTransacao;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class TransacaoResponseDto {
 
     private String ticker;
@@ -12,45 +16,5 @@ public class TransacaoResponseDto {
     private int quantidade;
     private LocalDate data;
     private TipoTransacao tipo;
-
-    public String getTicker() {
-        return ticker;
-    }
-
-    public void setTicker(String ticker) {
-        this.ticker = ticker;
-    }
-
-    public BigDecimal getPreco() {
-        return preco;
-    }
-
-    public void setPreco(BigDecimal preco) {
-        this.preco = preco;
-    }
-
-    public int getQuantidade() {
-        return quantidade;
-    }
-
-    public void setQuantidade(int quantidade) {
-        this.quantidade = quantidade;
-    }
-
-    public LocalDate getData() {
-        return data;
-    }
-
-    public void setData(LocalDate data) {
-        this.data = data;
-    }
-
-    public TipoTransacao getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(TipoTransacao tipo) {
-        this.tipo = tipo;
-    }
 
 }
