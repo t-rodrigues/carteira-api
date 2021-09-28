1effe12a537d16e7cd33dc61b0a19b21dabe281e
Thiago Rodrigues <thiagor_@live.com>
2021-09-27 22:10:26 -0300

    feat: add Transacao JpaRepository

--- src/main/java/dev/thiagorodrigues/carteira/infra/repositories/TransacaoRepository.java
+++ src/main/java/dev/thiagorodrigues/carteira/infra/repositories/TransacaoRepository.java
@@ -1,25 +1,11 @@
 package dev.thiagorodrigues.carteira.infra.repositories;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import dev.thiagorodrigues.carteira.domain.entities.Transacao;
 
 @Repository
-public class TransacaoRepository {
-
-    private List<Transacao> transacoes = new ArrayList<>();
-
-    public List<Transacao> findAll() {
-        return transacoes;
-    }
-
-    public Transacao save(Transacao transacao) {
-        transacoes.add(transacao);
-
-        return transacao;
-    }
+public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
 
 }
