16ec341b84ee1e8511a2014dff3555fc4d6ace3a
Thiago Rodrigues <thiagor_@live.com>
2021-09-27 21:10:43 -0300

    feat: Usuario mapped

--- src/main/java/dev/thiagorodrigues/carteira/domain/entities/Usuario.java
+++ src/main/java/dev/thiagorodrigues/carteira/domain/entities/Usuario.java
@@ -1,11 +1,19 @@
 package dev.thiagorodrigues.carteira.domain.entities;
 
+import javax.persistence.*;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String nome;
     private String email;
