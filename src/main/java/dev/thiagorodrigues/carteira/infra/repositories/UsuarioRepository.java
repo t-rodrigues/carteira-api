3985b5436b6e0fb7f7c8633445e2107e20674a34
Thiago Rodrigues <thiagor_@live.com>
2021-09-27 22:09:43 -0300

    feat: add Usuario JpaRepository

--- src/main/java/dev/thiagorodrigues/carteira/infra/repositories/UsuarioRepository.java
+++ src/main/java/dev/thiagorodrigues/carteira/infra/repositories/UsuarioRepository.java
@@ -1,25 +1,11 @@
 package dev.thiagorodrigues.carteira.infra.repositories;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import dev.thiagorodrigues.carteira.domain.entities.Usuario;
 
 @Repository
-public class UsuarioRepository {
-
-    private List<Usuario> usuarios = new ArrayList<>();
-
-    public List<Usuario> findAll() {
-        return usuarios;
-    }
-
-    public Usuario save(Usuario usuario) {
-        usuarios.add(usuario);
-
-        return usuario;
-    }
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 }
