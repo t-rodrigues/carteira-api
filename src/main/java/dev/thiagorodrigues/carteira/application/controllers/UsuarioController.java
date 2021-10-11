93dd13ce92e261521f7903b8a3a842741b8133f0
Thiago Rodrigues <thiagor_@live.com>
2021-10-11 19:09:22 -0300

    feat: add Swagger documentation

--- src/main/java/dev/thiagorodrigues/carteira/application/controllers/UsuarioController.java
+++ src/main/java/dev/thiagorodrigues/carteira/application/controllers/UsuarioController.java
@@ -3,6 +3,8 @@ package dev.thiagorodrigues.carteira.application.controllers;
 import dev.thiagorodrigues.carteira.application.dtos.UsuarioFormDto;
 import dev.thiagorodrigues.carteira.application.dtos.UsuarioResponseDto;
 import dev.thiagorodrigues.carteira.domain.services.UsuarioService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,16 +19,19 @@ import java.net.URI;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/usuarios")
+@Api(tags = "Usuários")
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
 
     @GetMapping
+    @ApiOperation("Listar usuários")
     public Page<UsuarioResponseDto> getUsuarios(Pageable paginacao) {
         return usuarioService.getUsuarios(paginacao);
     }
 
     @PostMapping
+    @ApiOperation("Cadastrar novo usuário")
     public ResponseEntity<UsuarioResponseDto> addUsuario(@RequestBody @Valid UsuarioFormDto usuarioFormDto,
             UriComponentsBuilder uriComponentsBuilder) {
         UsuarioResponseDto usuarioResponseDto = usuarioService.createUsuario(usuarioFormDto);
