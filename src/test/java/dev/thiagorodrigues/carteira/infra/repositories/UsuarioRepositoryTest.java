7bae346f7c56348f846dd2ef350c56b409561e01
Thiago Rodrigues <thiagor_@live.com>
2021-10-13 22:47:00 -0300

    feat: add UsuarioRepositoryTest

---
+++ src/test/java/dev/thiagorodrigues/carteira/infra/repositories/UsuarioRepositoryTest.java
@@ -0,0 +1,56 @@
+package dev.thiagorodrigues.carteira.infra.repositories;
+
+import dev.thiagorodrigues.carteira.domain.entities.Usuario;
+import dev.thiagorodrigues.carteira.domain.mocks.UsuarioFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@ActiveProfiles("test")
+class UsuarioRepositoryTest {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    private Usuario usuario;
+
+    @BeforeEach
+    private void setUp() {
+        usuario = UsuarioFactory.criarUsuarioSemId();
+
+        testEntityManager.persist(usuario);
+    }
+
+    @Test
+    void findByEmailDeveriaRetornarUmUsuarioValido() {
+        var usuarioEncontrado = usuarioRepository.findByEmail(usuario.getEmail());
+
+        assertEquals(usuario.getId(), usuarioEncontrado.getId());
+        assertEquals(usuario.getEmail(), usuarioEncontrado.getEmail());
+        assertEquals(usuario.getNome(), usuarioEncontrado.getNome());
+    }
+
+    @Test
+    void findByNaoDeveriaTerRetornoComEmailNaoCadastrado() {
+        var usuarioEncontrado = usuarioRepository.findByEmail("any@mail.com");
+
+        assertNull(usuarioEncontrado);
+    }
+
+}
