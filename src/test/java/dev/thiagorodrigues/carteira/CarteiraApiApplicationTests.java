09c89cb67a879ed692bd2dadd86ed687af11eb4e
Thiago Rodrigues <thiagor_@live.com>
2021-11-11 20:30:39 -0300

    test: fix test

--- src/test/java/dev/thiagorodrigues/carteira/CarteiraApiApplicationTests.java
+++ src/test/java/dev/thiagorodrigues/carteira/CarteiraApiApplicationTests.java
@@ -2,12 +2,14 @@ package dev.thiagorodrigues.carteira;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CarteiraApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
